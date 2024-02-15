32dda9c0093c3a5f7123f955c7491cd205ef491e
japneet <japneetsaini084@gmail.com>
2024-02-15 12:55:36 +0530

    canvas gamification

--- app/src/main/java/com/example/cse227/UNIT_2/Canvas/GameView.kt
+++ app/src/main/java/com/example/cse227/UNIT_2/Canvas/GameView.kt
@@ -5,14 +5,17 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.view.MotionEvent
 import android.view.View
 import com.example.cse227.R
 
 class GameView(ctx: Context): View(ctx) {
+    var q = 0;
+    var x = 0;
     var bg: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.mario_bg)
     var mario1: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.mario)
     var mario2: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.mario2)
-    var crow: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.crow)
+    var crow: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.crow)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         val rect1 = Rect(0,0,bg.width,bg.height)
@@ -20,7 +23,20 @@ class GameView(ctx: Context): View(ctx) {
 
         val rect3 = Rect(0,canvas.height-bg.height+370,mario1.width,canvas.height)
         canvas.drawBitmap(bg,rect1,rect2,null)
-        canvas.drawBitmap(mario2,0f,(canvas.height-bg.height+480).toFloat(),null)
+        canvas.drawBitmap(mario2,0f,(canvas.height-bg.height+480+q).toFloat(),null)
         canvas.drawBitmap(crow,100f,(canvas.height-bg.height-400).toFloat(),null)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+       if(event?.action == MotionEvent.ACTION_DOWN){
+           q -= 800
+           x += 20
+           invalidate()
+       }
+        if(event?.action == MotionEvent.ACTION_UP){
+            q += 800
+            invalidate()
+        }
+        return true
     }
 }
