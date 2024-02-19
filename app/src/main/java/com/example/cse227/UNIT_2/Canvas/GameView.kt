8ae20220ed29d06e618b238c5eb998f44c4086e2
japneet <japneetsaini084@gmail.com>
2024-02-19 12:27:24 +0530

    moving img vertically and horizontally

--- app/src/main/java/com/example/cse227/UNIT_2/Canvas/GameView.kt
+++ app/src/main/java/com/example/cse227/UNIT_2/Canvas/GameView.kt
@@ -15,7 +15,7 @@ class GameView(ctx: Context): View(ctx) {
     var bg: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.mario_bg)
     var mario1: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.mario)
     var mario2: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.mario2)
-    var crow: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.crow)
+    var crow: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.crow2)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         val rect1 = Rect(0,0,bg.width,bg.height)
@@ -24,7 +24,7 @@ class GameView(ctx: Context): View(ctx) {
         val rect3 = Rect(0,canvas.height-bg.height+370,mario1.width,canvas.height)
         canvas.drawBitmap(bg,rect1,rect2,null)
         canvas.drawBitmap(mario2,(0+x).toFloat(),(canvas.height-bg.height+480+q).toFloat(),null)
-        canvas.drawBitmap(crow,100f,(canvas.height-bg.height-400).toFloat(),null)
+        canvas.drawBitmap(crow,(900-x).toFloat(),(canvas.height-bg.height+920).toFloat(),null)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
